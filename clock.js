9189f7a4ca566901739b65e0efc00989bfa1f967
kooo8282 <70710735+kooo8282@users.noreply.github.com>
2020-11-02 21:08:35 +0900

    css

--- clock.js
+++ clock.js
@@ -8,7 +8,7 @@ function getTime() {
   const seconds = date.getSeconds();
   colckTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  }`;
 }
 
 function init() {
@@ -17,4 +17,4 @@ function init() {
 }
 
 init();
-document.title = "Wow fantastic baby~";
+document.title = "Momonton";
